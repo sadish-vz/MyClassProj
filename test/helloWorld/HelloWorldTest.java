ec18412f49a6e22bd80164b571d064851de70f85
sadish-vz <sadishkumar.x.neelamegam@verizon.com>
2015-09-29 10:20:56 -0700

    Changed the struct

---
+++ test/helloWorld/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package helloWorld;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import com.sk.pak.MyCl;
+
+public class HelloWorldTest {
+
+	
+
+	@Test
+	public void test() {
+		MyCl hello = new MyCl();
+		assertEquals("A test for Hello World String", "Hello World", hello.sayHello());
+	}
+
+}
